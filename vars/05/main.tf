ab8d4f8105e0ab026e00907d7d9354c158d99285
Pablo Ponce <47292039+pablosponce@users.noreply.github.com>
2022-11-05 20:11:56 +0100

    Update main.tf

--- vars/05/main.tf
+++ vars/05/main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "hola" {
   filename = var.fichero
-  content = ${random_string.mi_texto.result}
+  content = random_string.mi_texto.result
 }
 
 resource "random_string" "mi_texto" {
