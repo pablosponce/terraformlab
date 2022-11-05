6e9e0be6fc6daf1349bdff1888fc408e73b13735
Pablo Ponce <47292039+pablosponce@users.noreply.github.com>
2022-11-05 19:36:37 +0100

    Update variables.tf

--- vars/01/variables.tf
+++ vars/01/variables.tf
@@ -6,15 +6,3 @@ resource "local_file" "hola" {
 resource "random_string" "mi_texto" {
   length=var.length
 }
-pabloponce@Leolo var01 % cat variables.tf 
-variable "fichero" {
-  default = "/tmp/hola.txt"
-}
-
-variable "texto" {
-  default = "Hola mundo! (que poco original que eres)\n"
-}
-
-variable "length" {
-  default = 10
-}
